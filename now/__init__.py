9e8099bae0aed0c4b0a6bbdec419af060177d8b2
Florian Hönicke <hoenicke.florian@gmail.com>
2022-03-28 14:46:22 +0200

    feat: new version

--- now/__init__.py
+++ now/__init__.py
@@ -1,7 +1,7 @@
 import os as _os
 import sys as _sys
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __resources_path__ = _os.path.join(
     _os.path.dirname(_sys.modules['now'].__file__), 'resources'
 )
