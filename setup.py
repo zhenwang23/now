60eafa73883a37a1b88160c6b1a90708c8963951
Florian Hönicke <hoenicke.florian@gmail.com>
2022-04-07 14:28:19 +0200

    test: cpu linux

--- setup.py
+++ setup.py
@@ -34,10 +34,12 @@ with pathlib.Path('requirements_pip.txt').open() as requirements_txt:
         for requirement in pkg_resources.parse_requirements(requirements_txt)
     ]
 sys_platform = platform.system().lower()
-# if sys_platform == 'darwin':
-torch_requirement = 'torch==1.10.2'
-# else:
-#     torch_requirement = 'torch @ https://download.pytorch.org/whl/cpu/torch-1.10.2%2Bcpu-cp37-cp37m-linux_x86_64.whl'
+if sys_platform == 'darwin':
+    torch_requirement = 'torch==1.10.2'
+else:
+    torch_requirement = (
+        '-f https://download.pytorch.org/whl/torch_stable.html\ntorch==1.10.2+cpu'
+    )
 
 install_requires.append(torch_requirement)
 
