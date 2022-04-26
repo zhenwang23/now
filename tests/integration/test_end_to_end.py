0da2f1fb7100f78629601758ce03e4934a7a2f87
slettner <sebastian.lettner@jina.ai>
2022-04-26 12:53:14 +0200

    test(end2end): fix attribute name

--- tests/integration/test_end_to_end.py
+++ tests/integration/test_end_to_end.py
@@ -9,21 +9,21 @@ from now.dialog import NEW_CLUSTER
 
 
 @pytest.mark.parametrize(
-    'modality, dataset',
+    'output_modality, dataset',
     [('image', 'best-artworks'), ('image', 'deepfashion'), ('text', 'rock-lyrics')],
 )  # art, rock-lyrics -> no finetuning, fashion -> finetuning
 @pytest.mark.parametrize('quality', ['medium'])
 @pytest.mark.parametrize('cluster', [NEW_CLUSTER['value']])
 @pytest.mark.parametrize('new_cluster_type', ['local'])
 def test_backend(
-    modality: str,
+    output_modality: str,
     dataset: str,
     quality: str,
     cluster: str,
     new_cluster_type: str,
 ):
     kwargs = {
-        'modality': modality,
+        'output_modality': output_modality,
         'dataset': dataset,
         'quality': quality,
         'cluster': cluster,
