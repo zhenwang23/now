3bfae24f3ebb90b3a154b868020b51da37b8bc63
Florian Hönicke <hoenicke.florian@gmail.com>
2022-04-27 11:20:01 +0200

    fix: dialog

--- now/dialog.py
+++ now/dialog.py
@@ -149,7 +149,7 @@ def ask_data(user_input: UserInput, **kwargs):
 
 
 def prompt_plus(questions, attribute, **kwargs):
-    if kwargs and attribute in kwargs:
+    if kwargs and kwargs.get(attribute) is not None:
         return kwargs[attribute]
     else:
         answer = prompt(questions)
